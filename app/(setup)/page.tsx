774b9b672f6f98e2a79dc0f0ce8940929c619cad
Mai Đức Huy <huy.mai.set18@eiu.edu.vn>
2023-10-31 20:29:28 +0700

    first commit

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,26 @@
+import { initalProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
+
+const SetupPAge = async () => {
+  const profile = await initalProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPAge;
