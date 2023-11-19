481909a9c64aceef0e0ff5e5cd7ecf6720aea3e3
Mai Đức Huy <huy.mai.set18@eiu.edu.vn>
2023-11-19 17:54:59 +0700

    chat func added!

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 
 interface channelIdPageProps {
   params: {
@@ -37,13 +38,26 @@ const channelIdPage = async ({ params }: channelIdPageProps) => {
   }
 
   return (
-    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-screen">
       <ChatHeader
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
       />
-      <div className="flex-1">Future Messages</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type="channel"
