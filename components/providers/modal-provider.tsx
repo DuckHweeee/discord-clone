7bb7bbbf58a52f1e0c1fdedf9a05ef618c93d61c
Mai Đức Huy <huy.mai.set18@eiu.edu.vn>
2023-11-21 23:49:56 +0700

    DONE: fix the bugs for media rooms

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal";
-
 import { useEffect, useState } from "react";
+
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
-import { EditServerModal } from "@/components/modals/edit-sever-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
-import { LeaveServerModal } from "../modals/leave-server-modal";
-import { DeleteServerModal } from "../modals/delete-server-modal";
-import { DeleteChannelModal } from "../modals/delete-channel-modal";
-import { MessageFileModal } from "../modals/message-file-modal";
-import { DeleteMessageModal } from "../modals/delete-message-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -34,7 +35,7 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
-      <EditServerModal />
+      <EditChannelModal />
       <MessageFileModal />
       <DeleteMessageModal />
     </>
--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
+    });
+
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
     });
 
     return config;
